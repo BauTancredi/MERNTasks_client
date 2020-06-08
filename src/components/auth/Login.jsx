a926217457e53e8f3c96b6d9d19f6fab9c033f2c
BauTancredi <bautistatancredi@hotmail.com>
2020-06-08 20:23:23 -0300

    auth components

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <h1>Desde Login</h1>;
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = user;
+  const onChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Login</h1>
+
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              placeholder="Email"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              placeholder="Password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Login"
+            />
+          </div>
+        </form>
+
+        <Link to="/new-account" className="enlace-cuenta">
+          Obtain account
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/auth/NewAccount.jsx
+++ src/components/auth/NewAccount.jsx
@@ -1,7 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const NewAccount = () => {
-  return <h1>Desde New Account</h1>;
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirm: "",
+  });
+
+  const { name, email, password, confirm } = user;
+  const onChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    //No empty fields
+
+    //Password 6 charecter minimun
+
+    //Two passwods equals
+
+    //Action
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Register account</h1>
+
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Name"
+              value={name}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="confirmr">Confirm Password</label>
+            <input
+              type="password"
+              name="confirm"
+              id="confirm"
+              placeholder="Confirm Password"
+              value={confirm}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Register"
+            />
+          </div>
+        </form>
+
+        <Link to="/" className="enlace-cuenta">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NewAccount;
