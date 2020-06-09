6fbf9556f18bd410cf52e264bb55b70e8a184e81
BauTancredi <bautistatancredi@hotmail.com>
2020-06-09 19:55:39 -0300

    NavBar component

---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <header className="app-header">
+      <p className="nombre-usuario">
+        Hi <span>Juan Pablo</span>{" "}
+      </p>
+
+      <nav className="nav-principal">
+        <a href="#!">Log out</a>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Sidebar from "../layout/Sidebar";
+import NavBar from "../layout/NavBar";
 
 const Projects = () => {
   return (
     <div className="contenedor-app">
       <Sidebar />
       <div className="seccion-principal">
+        <NavBar />
         <main>
           <div className="contenedor-tareas"></div>
         </main>
