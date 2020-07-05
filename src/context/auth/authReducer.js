666fed181ef21fc2ea38c5d70c05db66d865ae2e
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-05 16:21:59 -0300

    axios config

--- src/context/auth/authReducer.js
+++ src/context/auth/authReducer.js
@@ -9,14 +9,6 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
-    case SHOW_ALERT:
-      return {
-        alert: action.payload,
-      };
-    case HIDE_ALERT:
-      return {
-        alert: null,
-      };
     default:
       return state;
   }
